9f67c795d4bd6e25f374d22a93332b6c24cba44c
Anastasiya <atse.anastasiya@gmail.com>
2023-07-22 19:15:53 +0000

    add faq text in locales/en and faq/index

--- components/page-modules/faq/index.tsx
+++ components/page-modules/faq/index.tsx
@@ -1,10 +1,16 @@
+'use client'
 import { FaqAccordion } from './components/faq-accordion';
+import {useParams} from 'next/navigation';
+import {useTranslation} from '@/app/i18n/client';
 
 export function Faq() {
+  const params = useParams();
+  const { t } = useTranslation(params.lng);
+
   return (
     <div className='bg-[#F1F1F1]'>
       <div className='lg:w-[900px] mx-auto py-10 px-4'>
-        <h1 className='text-[32px] font-bold text-center'>Frequently Asked Questions</h1>
+        <h1 className='text-[32px] font-bold text-center'>{t('faq-header-text')}</h1>
 
         <FaqAccordion />
       </div>
